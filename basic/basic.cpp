24259f45759f3e54ad8bf3fbaa974389a62da8c8
Giuliano Motter <mottergiuliano@gmail.com>
2024-09-17 22:53:01 -0300

    Apply some basic smoothing and filtering

--- basic/basic.cpp
+++ basic/basic.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
-    std::string image_path = "images/pool_table.png";
-    cv::Mat     image      = cv::imread(image_path, cv::IMREAD_COLOR);
-    cv::imshow("Display window", image);
+    std::string img_path = "images/pool_table.png";
+    cv::Mat     img      = cv::imread(img_path, cv::IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not load img!" << std::endl;
+        return -1;
+    }
+    cv::imshow("Display window", img);
     cv::waitKey(0);
     return 0;
 }
---
+++ simple_filter/simple_filter.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main()
+{
+    std::string img_path = "images/flower.jpg";
+    cv::Mat     img      = cv::imread(img_path, cv::IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not load img!" << std::endl;
+        return -1;
+    }
+
+    int     new_w        = 500;
+    float   aspect_ratio = static_cast<float>(img.cols) / static_cast<float>(img.rows);
+    int     new_h = static_cast<int>(new_w / aspect_ratio);
+    cv::Mat resized_img;
+    cv::resize(img, resized_img, cv::Size(new_w, new_h));
+
+    cv::Mat median_blur;
+    cv::medianBlur(resized_img, median_blur, 9);
+
+    cv::Mat gaussian_blur;
+    cv::GaussianBlur(resized_img, gaussian_blur, cv::Size(1, 1), 9, 9);
+
+    cv::imshow("Original img", resized_img);
+    cv::imshow("median blur", median_blur);
+    cv::imshow("gaussian blur", gaussian_blur);
+    cv::waitKey(0);
+    return 0;
+}
